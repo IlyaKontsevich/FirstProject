a1ee948ca18dc5315688163385f61dd611d7324d
IlyaKontsevich <i_koncevich1999@mail.ru>
2019-07-08 14:23:37 +0300

    return all

---
+++ src/com/internship/model/User.java
@@ -0,0 +1,40 @@
+package com.internship.model;
+
+import java.util.Scanner;
+
+public class User {
+    private String name;
+
+
+    public String GetName(){
+        return name;
+    }
+
+    public User(){
+        Scanner scaner = new Scanner(System.in);
+
+        System.out.println("Enter user name: ");
+        name = scaner.nextLine();
+        System.out.println("User successfully add");
+    }
+
+}
+//1.Naming convention
+//2.Pakets
+//3.github .idea .out *.iml + ignore: campiled; IDE; build
+//4.RAEDME.md
+//store
+//!
+//Dao  CRUD user or task
+//!
+//Service
+//!
+//console controller + validation
+//by.exapel.model.User
+//          .console.M
+//          .service.
+//          .dao
+
+
+
+//Task: user.id

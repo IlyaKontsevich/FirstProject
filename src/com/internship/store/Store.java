03a6c977c5b49191e6deb557462832ccafc19fc5
IlyaKontsevich <52547231+IlyaKontsevich@users.noreply.github.com>
2019-07-09 20:27:34 +0300

    Delete Store.java

--- src/com/internship/store/Store.java
+++
@@ -1,9 +0,0 @@
-package com.internship.store;
-
-import com.internship.model.*;
-import java.util.*;
-
-public class Store {
-    public Map<String, Task> tasks = new HashMap();
-    public Map<String, User> users = new HashMap();
-}
